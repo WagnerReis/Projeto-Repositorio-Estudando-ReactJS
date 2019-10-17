c980e097dea4fe7884776e05f6aa0616ed03a223
Wagner Reis <wagnerreis1997@gmail.com>
2019-10-17 16:33:09 -0300

    Listando repositorios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,67 +1,94 @@
-import React, { useState, useCallback } from 'react';
-import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
-import { Container, Form, SubmitButton } from './styles';
+import React, {useState, useCallback} from 'react';
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
+import {Container, Form, SubmitButton, List, DeleteButton} from './styles';
 
 import api from '../../services/api';
 
-export default function Main() {
+export default function Main(){
 
-    const [newRepo, setNewRepo] = useState('');
-    const [repositorios, setRepositorios] = useState('');
-    const [loading, setLoading] = useState(false);
+  const [newRepo, setNewRepo] = useState('');
+  const [repositorios, setRepositorios] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const handleSubmit = useCallback((e) => {
-        e.preventDefault();
 
-        async function submit() {
-            setLoading(true);
-            try {
-                const response = await api.get(`repos/${newRepo}`);
+  const handleSubmit = useCallback((e)=>{
+    e.preventDefault();
 
-                const data = {
-                    name: response.data.full_name,
-                }
-
-                setRepositorios([...repositorios, data]);
-                setNewRepo('');
-            }catch(error){
-                console.log(error);  
-            }finally{
-                setLoading(false);
-            }
-        
+    async function submit(){
+      setLoading(true);
+      try{
+        const response = await api.get(`repos/${newRepo}`);
+  
+        const data = {
+          name: response.data.full_name,
         }
+    
+        setRepositorios([...repositorios, data]);
+        setNewRepo('');
+      }catch(error){
+        console.log(error);
+      }finally{
+        setLoading(false);
+      }
 
-        submit();
-
-    }, [newRepo, repositorios]);
-
-    function handleinputChange(e) {
-        setNewRepo(e.target.value);
     }
 
-    return (
-        <Container>
-            <h1>
-                <FaGithub size={25} />
-                Meus Repositorios
-            </h1>
+    submit();
 
-            <Form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Adicionar Repositorios"
-                    value={newRepo}
-                    onChange={handleinputChange}
-                />
+  }, [newRepo, repositorios]);
 
-                <SubmitButton loading={loading ? 1 : 0}>
-                    {loading ? (
-                        <FaSpinner color="#FFF" size={14}/>
-                    ) : (
-                        <FaPlus color="#FFF" size={14} />
-                    )}
-                </SubmitButton>
+  function handleinputChange(e){
+    setNewRepo(e.target.value);
+  }
 
-            </Form>
-        </Container>
-    );
+  const handleDelete = useCallback((repo)=> {
+    const find = repositorios.filter(r => r.name !== repo);
+    setRepositorios(find);
+  }, [repositorios]);
+
+
+  return(
+    <Container>
+      
+      <h1>
+        <FaGithub size={25}/>
+        Meus Repositorios
+      </h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        placeholder="Adicionar Repositorios"
+        value={newRepo}
+        onChange={handleinputChange}
+        />
+
+        <SubmitButton loading={loading ? 1 : 0}>
+          {loading ? (
+            <FaSpinner color="#FFF" size={14}/>
+          ) : (
+            <FaPlus color="#FFF" size={14}/>
+          )}
+        </SubmitButton>
+
+      </Form>
+
+      <List>
+         {repositorios.map(repo => (
+           <li key={repo.name}>
+             <span>
+             <DeleteButton onClick={()=> handleDelete(repo.name) }>
+                <FaTrash size={14}/>
+             </DeleteButton>  
+             {repo.name}
+             </span>
+             <a href="">
+               <FaBars size={20}/>
+             </a>
+           </li>
+         ))} 
+      </List>
+
+    </Container>
+  )
 }
