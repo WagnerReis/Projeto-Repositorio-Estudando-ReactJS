9ad6d33899eb5ed2fede77a1a8794211e867c282
Wagner Reis <wagnerreis1997@gmail.com>
2019-10-17 17:43:17 -0300

    Detalhes do repositorio

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,7 @@
 import React, {useState, useCallback, useEffect} from 'react';
 import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
 import {Container, Form, SubmitButton, List, DeleteButton} from './styles';
-
+import {Link} from 'react-router-dom';
 import api from '../../services/api';
 
 export default function Main(){
@@ -111,9 +111,9 @@ export default function Main(){
              </DeleteButton>  
              {repo.name}
              </span>
-             <a href="">
+             <Link to={`/repositorio/${encodeURIComponent(repo.name)}`}>
                <FaBars size={20}/>
-             </a>
+             </Link>
            </li>
          ))} 
       </List>
